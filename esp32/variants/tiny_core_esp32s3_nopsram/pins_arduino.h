197b5362abc04724f7adfbfa5ca551f44f0f0918
Geoff McIntyre <macgeoffrey123@gmail.com>
2025-02-15 16:57:36 -0700

    Updates board name to "tinyCore"

---
+++ esp32/variants/tiny_core_esp32s3_nopsram/pins_arduino.h
@@ -0,0 +1,59 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+#include "soc/soc_caps.h"
+
+#define USB_VID          0x239A
+#define USB_PID          0x8113
+#define USB_MANUFACTURER "Mister Industries"
+#define USB_PRODUCT      "tinyCore ESP32-S3 No PSRAM"
+#define USB_SERIAL       ""  // Empty string for MAC address
+
+// User LED
+#define LED_BUILTIN 33
+#define BUILTIN_LED LED_BUILTIN  // Maps to the SIG LED Pin
+
+#define I2C_POWER         6     // I2C power pin
+#define PIN_I2C_POWER     6     // I2C power pin
+
+static const uint8_t TX = 39;
+static const uint8_t RX = 38;
+#define TX1 TX
+#define RX1 RX
+
+static const uint8_t SDA = 3;
+static const uint8_t SCL = 4;
+
+static const uint8_t SS = 1;
+static const uint8_t MOSI = 35;
+static const uint8_t SCK = 36;
+static const uint8_t MISO = 37;
+
+static const uint8_t A0 = 18;
+static const uint8_t A1 = 17;
+static const uint8_t A2 = 16;
+static const uint8_t A3 = 15;
+static const uint8_t A4 = 14;
+static const uint8_t A5 = 7;
+static const uint8_t A6 = 3;
+static const uint8_t A7 = 4;
+static const uint8_t A8 = 5;
+static const uint8_t A9 = 9;
+static const uint8_t A10 = 10;
+static const uint8_t A11 = 11;
+static const uint8_t A12 = 12;
+static const uint8_t A13 = 13;
+
+static const uint8_t T3 = 3;
+static const uint8_t T4 = 4;
+static const uint8_t T5 = 5;
+static const uint8_t T8 = 8;
+static const uint8_t T9 = 9;
+static const uint8_t T10 = 10;
+static const uint8_t T11 = 11;
+static const uint8_t T12 = 12;
+static const uint8_t T13 = 13;
+static const uint8_t T14 = 14;
+
+#endif /* Pins_Arduino_h */
